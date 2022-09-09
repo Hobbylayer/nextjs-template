7e8b93a70c0d0018d83e9ba47435f5715454d261
Javier A Mora Gil <j4viermora@gmail.com>
2022-09-09 00:47:37 -0400

    add initial configs

---
+++ README.md
@@ -0,0 +1,21 @@
+# CONDOMINIO WEB APP FRONTEND
+
+Este proyecto esta construido
+
+- Nextjs
+- Axios
+- Material UI
+
+## Enviroment
+
+Debe asegurarte de tener node 14 o superior instalado y luego instalar los modulos de node
+
+```bash
+npm install
+```
+
+Ahora para levantar el proyecto
+
+```bash
+npm run dev
+```
--- src/navigation/vertical/index.ts
+++ src/navigation/vertical/index.ts
@@ -1,14 +1,13 @@
 // ** Icon imports
-import Login from 'mdi-material-ui/Login'
 import Table from 'mdi-material-ui/Table'
 import CubeOutline from 'mdi-material-ui/CubeOutline'
 import HomeOutline from 'mdi-material-ui/HomeOutline'
 import FormatLetterCase from 'mdi-material-ui/FormatLetterCase'
-import AccountCogOutline from 'mdi-material-ui/AccountCogOutline'
 import CreditCardOutline from 'mdi-material-ui/CreditCardOutline'
-import AccountPlusOutline from 'mdi-material-ui/AccountPlusOutline'
-import AlertCircleOutline from 'mdi-material-ui/AlertCircleOutline'
-import GoogleCirclesExtended from 'mdi-material-ui/GoogleCirclesExtended'
+
+// ** React icons imports
+import { MdAccountBalance, MdDescription, MdNoteAdd, MdRequestQuote, MdLocationPin, MdSettings, MdPeople, MdPool, MdReportProblem } from "react-icons/md";
+
 
 // ** Type import
 import { VerticalNavItemsType } from 'src/@core/layouts/types'
@@ -16,48 +15,71 @@ import { VerticalNavItemsType } from 'src/@core/layouts/types'
 const navigation = (): VerticalNavItemsType => {
   return [
     {
-      title: 'Dashboard',
+      title: 'Inicio',
       icon: HomeOutline,
-      path: '/'
+      path: '/app'
     },
     {
-      title: 'Account Settings',
-      icon: AccountCogOutline,
-      path: '/account-settings'
+      sectionTitle: 'Transacciones'
     },
     {
-      sectionTitle: 'Pages'
+      title: 'Solicitudes de pago',
+      icon: MdRequestQuote,
+      path: '/app/payments-requests'
+
+      // openInNewTab: true
     },
     {
-      title: 'Login',
-      icon: Login,
-      path: '/pages/login',
+      title: 'Ingresos',
+      icon: MdNoteAdd,
+      path: '/app/income',
       openInNewTab: true
     },
     {
-      title: 'Register',
-      icon: AccountPlusOutline,
-      path: '/pages/register',
+      title: 'Gastos',
+      icon: MdDescription,
+      path: '/app/expenses',
       openInNewTab: true
     },
     {
-      title: 'Error',
-      icon: AlertCircleOutline,
-      path: '/pages/error',
+      title: 'Bancos',
+      icon: MdAccountBalance,
+      path: '/app/banks-accont',
       openInNewTab: true
     },
+    { sectionTitle: 'Condominio' },
     {
-      sectionTitle: 'User Interface'
+      title: 'Locaciones',
+      icon: MdLocationPin,
+      path: '/app/locations',
+    },
+    {
+      title: 'Residentes',
+      icon: MdPeople,
+      path: '/app/residents',
+    },
+    {
+      title: 'Areas comunes',
+      icon: MdPool,
+      path: '/app/commons-areas',
+    },
+    {
+      title: 'Incidencias',
+      icon: MdReportProblem,
+      path: '/app/incidents',
+    },
+    {
+      title: 'Configuracion',
+      icon: MdSettings,
+      path: '/app/locations',
+    },
+    {
+      sectionTitle: 'Reports'
     },
     {
       title: 'Typography',
       icon: FormatLetterCase,
       path: '/typography'
-    },
-    {
-      title: 'Icons',
-      path: '/icons',
-      icon: GoogleCirclesExtended
     },
     {
       title: 'Cards',
@@ -73,7 +95,8 @@ const navigation = (): VerticalNavItemsType => {
       icon: CubeOutline,
       title: 'Form Layouts',
       path: '/form-layouts'
-    }
+    },
+
   ]
 }
 
